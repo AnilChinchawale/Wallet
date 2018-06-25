0bd62fa53ad5283b199787bc060d373b693af103
sanatkumarsjk <sanatkumarsjk@gmail.com>
2018-06-25 17:36:45 +0530

    Get status API Inntegrated

--- My_XDC_E-Wallet_API/routes/txStatus (copy).js
+++ My_XDC_E-Wallet_API/routes/txStatus (copy).js
@@ -4,12 +4,12 @@ var router = express.Router();
 var cors = require('cors');
 var router = express.Router();
 
-router.post('/', cors(), function(req, res) {
+router.get('/', cors(), function(req, res) {
 	console.log("in texStatus");
     var web3 = Web3.initiateWeb3();
     console.log("body::::::::::::: ");
     var receipt = web3.eth.getTransaction("0x1cf824662fda2bb33fb526e42c91a48a98f642494bdce398db10371eb04ee6d3");
-	console.log("in texStatus",receipt);
+
     res.status(200).json(receipt);
 });
 
--- etherwallet/app/scripts/nodeHelpers/etherscan.js
+++ etherwallet/app/scripts/nodeHelpers/etherscan.js
@@ -29,13 +29,30 @@ etherscan.getBalance = function(addr, callback) {
     });
 }
 etherscan.getTransaction = function(txHash, callback) {
-    this.post({
+    this.post1({
         module: 'proxy',
         action: 'eth_getTransactionByHash',
         txhash: txHash,
     }, function(data) {
+        console.log("data eth_getTransactionByHash :::::::::::::::::::  ",data);
+        /*{
+  "blockHash": "0xe3738974a0587a5d426aea1d486fd9df4f41b88c8b0d958d374cdb11f59dd98d",
+  "blockNumber": "0x594478",
+  "from": "0x3a456874fc9dd9867d14ca9fead9719010958468",
+  "gas": "0x5208",
+  "gasPrice": "0xee6b2800",
+  "hash": "0x429c7db6fab7d98db464512c76849db8cfb36d7f2de043d3f24cdb59cea3c81e",
+  "input": "0x",
+  "nonce": "0xa",
+  "to": "0x915d7915f2b469bb654a7d903a5d4417cb8ea7df",
+  "transactionIndex": "0x64",
+  "value": "0x1f0d9be587bb0000",
+  "v": "0x26",
+  "r": "0x54e52032e76186986dafb1d06673c88e747846c5b6a044128b1251f4aff8fc6",
+  "s": "0x2f285e554b89fc6c61442d23b7869129a7f760617e778fc9140e8a17ed3c92df"
+}*/
         if (data.error) callback({ error: true, msg: data.error.message, data: '' });
-        else callback({ error: false, msg: '', data: data.result });
+        else callback({ error: false, msg: '', data: data});
     });
 }
 etherscan.getTransactionData = function(addr, callback) {
@@ -111,6 +128,8 @@ etherscan.queuePost = function() {
     var callback = this.pendingPosts[0].callback;
     var parentObj = this;
     data.apikey = 'DSH5B24BQYKD1AD8KUCDY3SAQSS6ZAU175';
+    
+    console.log("this.SERVERURL"   , this.SERVERURL,"data"   , data, "this .config   ", this.config);
     ajaxReq.http.post(this.SERVERURL, ajaxReq.postSerializer(data), this.config).then(function(data) {
         callback(data.data);
         parentObj.pendingPosts.splice(0, 1);
@@ -128,4 +147,32 @@ etherscan.post = function(data, callback) {
     });
     if (this.pendingPosts.length == 1) this.queuePost();
 }
+
+etherscan.queuePost1 = function() {
+    var data = this.pendingPosts[0].data;
+    var callback = this.pendingPosts[0].callback;
+    var parentObj = this;
+    data.apikey = 'DSH5B24BQYKD1AD8KUCDY3SAQSS6ZAU175';
+    
+    var SERVERURL="http://90.0.0.68:35108/txStatus";
+    console.log("this.SERVERURL::::::::::"   , this.SERVERURL,"data"   , data, "this .config   ", this.config);
+    ajaxReq.http.post(SERVERURL, ajaxReq.postSerializer(data), this.config).then(function(data) {
+        callback(data.data);
+        parentObj.pendingPosts.splice(0, 1);
+        if (parentObj.pendingPosts.length > 0) parentObj.queuePost1();
+    }, function(data) {
+        callback({ error: true, msg: "connection error", data: "" });
+    });
+}
+etherscan.post1 = function(data, callback) {
+    this.pendingPosts.push({
+        data: data,
+        callback: function(_data) {
+            callback(_data);
+        }
+    });
+    if (this.pendingPosts.length == 1) this.queuePost1();
+}
+
+
 module.exports = etherscan;
